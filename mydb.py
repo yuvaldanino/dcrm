1dc7b86d9cf27e427f57c1a59e26de546d1439da
Yuval Danino <daninoyuval@gmail.com>
2023-11-01 19:43:24 -0700

    first commit

---
+++ mydb.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+#shows  version of mysql connector 
+#print(mysql.connector.__version__)
+
+dataBase = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "yuval1234"
+)
+
+#new cursor object 
+cursorObject = dataBase.cursor()
+
+#create database 
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("All done!")
